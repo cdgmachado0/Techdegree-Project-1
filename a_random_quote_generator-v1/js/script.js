a1e1e08f81bc19ab4549c845513c426f41f19939
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2020-11-26 01:56:39 +0100

    Finished with quotes array

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -10,7 +10,32 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  {
+    quote: "The way to get started is to quit talking and begin doing",
+    source: "Walt Disney"
+  },
+  {
+    quote: "If life were predictable it would cease to be life, and be without flavor",
+    source: "Eleanor Roosevelt"
+  },
+  {
+    quote: "The greatest glory in living lies not in never falling, but in rising every time we fall",
+    source: "Nelson Mandela"
+  },
+  {
+    quote: "Now is the time to make justice a reality for all of God’s children.",
+    source: "Martin Luther King Jr.",
+    citation: "'I have a dream' speech",
+    year: 1963
+  },
+  {
+    quote: "Your time is limited, so don't waste it living someone else's life",
+    source: "Steve Jobs",
+    citation: "Standford Commencement Speech",
+    year: 2005
+  }
+];
 
 
 /***
